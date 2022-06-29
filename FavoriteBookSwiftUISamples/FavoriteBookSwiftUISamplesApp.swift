ecce47926b6cca3380f1b58b10d2b7348256139f
ergeakova <ergeak@gmail.com>
2022-06-30 02:44:36 +0300

    Initial Commit

---
+++ FavoriteBookSwiftUISamples/FavoriteBookSwiftUISamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavoriteBookSwiftUISamplesApp.swift
+//  FavoriteBookSwiftUISamples
+//
+//  Created by Erge Gevher Akova on 30.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct FavoriteBookSwiftUISamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
